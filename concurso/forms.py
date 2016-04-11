e4eae245a493dae9ae125369285a6e617743e7de
Luis <luiko.gmolina@gmail.com>
2016-04-11 12:28:03 +0100

    Refactor login form and template

--- concurso/forms.py
+++ concurso/forms.py
@@ -1,15 +1,15 @@
 from flask.ext.wtf import Form
 from wtforms import (StringField, BooleanField, TextField,
          validators, DateField, SubmitField)
-from wtforms.validators import DataRequired
 
 
 class Regist_Form(Form):
-    email = StringField('Email', validators=[DataRequired()])
-    nickname = StringField('Nickname', validators=[DataRequired()])
+    email = StringField('Email', validators=[validators.DataRequired()])
+    nickname = StringField('Nickname', validators=[validators.DataRequired()])
     birthday = DateField('Birthday')
     submit = SubmitField("Sign Up")
 
 
 class Login_Form(Form):
-    email = StringField('email')
+    email = StringField('Email', validators=[validators.DataRequired()])
+    submit = SubmitField("Login")
